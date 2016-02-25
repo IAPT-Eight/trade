ec63bd2602877d80f2b4bb5f843af68db686a6c0
Alfio Emanuele Fresta <alfio.emanuele.f@gmail.com>
2016-02-25 12:34:27 +0000

    Working proposals and counter-proposals

--- applications/trade/controllers/trade.py
+++ applications/trade/controllers/trade.py
@@ -1,6 +1,18 @@
 from datetime import datetime
 
 
+def _get_form_list(input_name, form):
+    """
+    Given the name of a multipart-form list,
+     always returns a list of numeric IDs.
+    """
+    input_variable = "%s[]" % input_name
+    if isinstance(form.vars[input_variable], list):
+        return [long(x) for x in form.vars[input_variable]]
+    else:
+        return [long(form.vars[input_variable])]
+
+
 @auth.requires_login()
 def index():
     """
@@ -35,9 +47,113 @@ def new():
      to make a counter-proposal to (which pre-populates the form w/ the old stuff).
     """
 
-    proposal_form = SQLFORM(db.trade_proposal, fields=[
-        'sender_items', 'receiver_items',
-    ])
+    receiver = db.auth_user[request.args(0)]
+    old_proposal = db.trade_proposal[request.args(1)]
+
+    # Get the list of items that can be selected for the proposal.
+    sender_items = db((db.item.owner_ref==auth.user)&(db.item.list_type==LIST_TRADING)).select(db.item.ALL)
+    receiver_items = db((db.item.owner_ref==receiver)&(db.item.list_type==LIST_TRADING)).select(db.item.ALL)
+
+    selected_sender_items = []
+    selected_receiver_items = []
+
+    if old_proposal:  # If counter-proposing, pre-select the items.
+        selected_receiver_items = old_proposal.sender_items
+        selected_sender_items = old_proposal.receiver_items
+
+    proposal_form = FORM(
+        DIV(
+            DIV(
+                DIV(  # Sender Items
+                    LABEL(
+                        "What would you like to give to %s?" % receiver.username,
+                        _class="control-label"
+                    ),
+                    DIV(
+                        P(
+                            *[
+                                P(LABEL(
+                                    INPUT(
+                                            _name="sender_items[]",
+                                            _value=item.id,
+                                            _type="checkbox",
+                                            _checked=(item.id in [y.id for y in selected_sender_items]),
+                                            requires=(
+                                                IS_NOT_EMPTY(),
+                                            )),
+                                    item.name,
+                                ),)
+                                for item in sender_items
+                            ]),
+                        _class=""
+                    ),
+                    _class="form-group",
+                ),
+                _class="col-md-6",
+            ),
+            DIV(
+                DIV(  # Receiver Items
+                    LABEL(
+                        "What would you like from %s?" % receiver.username,
+                        _class="control-label"
+                    ),
+                    DIV(
+                        P(
+                            *[
+                                P(LABEL(
+                                    INPUT(
+                                            _name="receiver_items[]",
+                                            _value=item.id,
+                                            _type="checkbox",
+                                            _checked=(item.id in [y.id for y in selected_receiver_items]),
+                                            requires=(
+                                                IS_NOT_EMPTY(),
+                                            )),
+                                    item.name,
+                                ),)
+                                for item in receiver_items
+                            ]),
+                        _class=""
+                    ),
+                    _class="form-group",
+                ),
+                _class="col-md-6",
+            ),
+            _class="row"
+        ),
+        BUTTON(
+            I(_class="fa fa-fw fa-check"),
+            "Send this proposal",
+            _type="submit",
+            _class="btn btn-block btn-primary",
+        ),
+        record=None,
+        keepvalues=True,
+    )
+
+    # If the form is completed and valid
+    if proposal_form.accepts(request, session):
+
+        # Collets all of the data for the proposal
+        sender_item_ids = _get_form_list('sender_items', proposal_form)
+        receiver_item_ids = _get_form_list('receiver_items', proposal_form)
+        sender = auth.user
+        receiver = receiver
+        status = WAITING
+
+        # Insert the proposal in the database
+        db.trade_proposal.insert(
+            status=status, sender=sender, receiver=receiver,
+            sender_items=sender_item_ids, receiver_items=receiver_item_ids
+        )
+
+        # If this is a counter-proposal, decline the old.
+        if old_proposal:
+            old_proposal.update_record(
+                status=DECLINED
+            )
+
+        redirect(URL('trade', 'trade', 'index'))
 
     return {
         "proposal_form": proposal_form
