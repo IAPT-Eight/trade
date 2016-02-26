6e185df4f323af5ac7e23a4d3aac079a8fa00e46
Katie Mills <km854@york.ac.uk>
2016-02-26 23:57:50 +0000

    Implemented category filtering

--- applications/trade/controllers/default.py
+++ applications/trade/controllers/default.py
@@ -2,23 +2,32 @@
 # this file is released under public domain and you can use without limitations
 
 #########################################################################
-## This is a sample controller
-## - index is the default action of any application
-## - user is required for authentication and authorization
-## - download is for downloading files uploaded in the db (does streaming)
+## The homepage of the website
 #########################################################################
 
 def index():
 	if request.args(0) == None:
 		search_vals = None
-	else: 
-		search_vals = request.args(0).split('_')
+		search_query = True
+	else:
+		search_vals = request.args(0).split('_') # TODO - implement search
+		search_query = True #TODO	
+	
+	if request.vars.cat != None:
+		category_query  = db.category.name == request.vars.cat
+	else:
+		category_query = True
 		
+	privacy_query = db.list_item_type != 2 # ("private" not in db.list_item_type.name.lower()
+	list_join = db.list_item_type.id == db.item.list_type
+	category_join = db.item.categories == db.category.id
+	
+	items = db(list_join & category_join & search_query & category_query & privacy_query).select(
+		db.item.name, db.item.image, db.item.item_value, db.item.id, db.item.categories)
+	
 	
 	categories_as_dicts = db(db.category).select(db.category.name).as_list()
 	categories_as_list = [cat['name'] for cat in categories_as_dicts]
-	
-	items = None
 	return dict(search_vals=search_vals, categories=categories_as_list, items=items)
 
 
