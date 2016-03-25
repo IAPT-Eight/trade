5d6e5df50f70bf75613b8e7fb244c12b7d83797a
Katie Mills <km854@york.ac.uk>
2016-03-25 23:02:22 +0000

    Added (current) after selected categories on homepage and user page. Renamed a couple of filter categories and removed fixed width from Private Collection icon to make room

--- applications/trade/models/z_categories.py
+++ applications/trade/models/z_categories.py
@@ -1,6 +1,6 @@
 if db(db.category.id > 0).count() == 0:
-    categories = ['Advertising and brand', 'Architectural', 'Art', 'Books, magazines, paper',
-                  'Clothing, fabric, textiles', 'Coins, currency, stamps', 'Film and television',
+    categories = ['Advertising and brand', 'Architectural', 'Art', 'Books and magazines',
+                  'Clothing, fabric, textiles', 'Money and stamps', 'Film and television',
                   'Glass and pottery', 'Household items', 'Memorabilia', 'Music', 'Nature and animals',
                   'Sports', 'Technology', 'Themed', 'Toys and games', 'Other']
     for category in categories:
